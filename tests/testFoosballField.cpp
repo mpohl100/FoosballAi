191c8a0bd3882a4dcee3c69ccea8fbb55c7d17b9
Michael Pohl <mpohl100@googlemail.com>
2020-07-26 09:09:04 +0200

    Koordinatensystem im Sterntest richtig verwenden (-240 offset)

--- tests/testFoosballField.cpp
+++ tests/testFoosballField.cpp
@@ -3,14 +3,14 @@
 int main()
 {
     std::vector<Shot> star={
-        Shot(Ball(600,340), Ball(600, 600)),
-        Shot(Ball(600,340), Ball(600, 0)),
-        Shot(Ball(600,340), Ball(1200, 340)),
-        Shot(Ball(600,340), Ball(0, 340)),
-        Shot(Ball(600,340), Ball(1200, 600)),
-        Shot(Ball(600,340), Ball(0, 600)),
-        Shot(Ball(600,340), Ball(1200, 0)),
-        Shot(Ball(600,340), Ball(0, 0)),
+        Shot(Ball(600,100), Ball(600, 600)),
+        Shot(Ball(600,100), Ball(600, -240)),
+        Shot(Ball(600,100), Ball(1200, 100)),
+        Shot(Ball(600,100), Ball(0, 100)),
+        Shot(Ball(600,100), Ball(1200, 680)),
+        Shot(Ball(600,100), Ball(0, 600)),
+        Shot(Ball(600,100), Ball(1200, -240)),
+        Shot(Ball(600,100), Ball(0, -240)),
     };
     FoosballField field(star, {});
     field.draw(std::cout); 
